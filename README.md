2405aa0ef42c8c4f759342d08ee8ff88ab958943
DailaYoffey <DanilaYoffey@outlook.com>
2024-03-04 00:48:51 -0500

    initial booking system

---
+++ README.md
@@ -0,0 +1,30 @@
+# React Appointment App
+
+This is a simple React app that allows users to schedule appointments. It makes use of React, React Icons, and Tailwind CSS.
+
+## Installation
+
+1. Clone the repository.
+2. Navigate to the project directory and run `npm install`.
+3. Run `npm start` to start the app.
+
+## Usage
+
+1. Once the app is running, navigate to the homepage to view a list of current appointments.
+2. To add a new appointment, click the "Add Appointment" button and fill in the necessary information.
+3. To edit an existing appointment, click the edit button next to the appointment and make your changes.
+4. To delete an appointment, click the delete button next to the appointment.
+
+## Technologies Used
+
+- React
+- React Icons
+- Tailwind CSS
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+This project is licensed under the [MIT](https://choosealicense.com/licenses/mit/) License.
---
+++ src/App.js
@@ -0,0 +1,95 @@
+import { BiCalendar } from "react-icons/bi";
+import { useEffect, useState, useCallback } from "react";
+import AddAppointment from "./components/AddAppointment";
+import AppointmentInfo from "./components/AppointmentInfo";
+import Search from "./components/Search";
+
+function App() {
+  let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState("");
+  let [sortBy, setSortBy] = useState("petName");
+  let [orderBy, setOrderBy] = useState("asc");
+
+  const filteredAppointment = appointmentList
+    .filter((item) => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      );
+    })
+    .sort((a, b) => {
+      let order = orderBy === "asc" ? 1 : -1;
+      return a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
+        ? -1 * order
+        : 1 * order;
+    });
+
+  const fetchData = useCallback(() => {
+    fetch("./data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setAppointmentList(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return (
+    <>
+      <div className="App container mx-auto mt-3 font-thin">
+        <h1 className="text-5xl mb-4">
+          <BiCalendar className="inline-block text-red-400 align-top" />
+          Your Appointments
+        </h1>
+        <AddAppointment
+          onSendAppointment={(appointment) => {
+            setAppointmentList([...appointmentList, appointment]);
+          }}
+          lastId={appointmentList.reduce(
+            (pre, curr) => (Number(curr.id) > pre ? Number(curr.id) : pre),
+            0
+          )}
+        />
+        <Search
+          query={query}
+          onQueryChange={(event) => {
+            setQuery(event.target.value);
+          }}
+          orderBy={orderBy}
+          onOrderByChange={(val) => {
+            setOrderBy(val);
+          }}
+          sortBy={sortBy}
+          onSortBYChange={(val) => {
+            setSortBy(val);
+          }}
+        />
+        <ul className="divide-y divide-gray-200">
+          {filteredAppointment.map((appointment) => {
+            return (
+              <AppointmentInfo
+                onDeleteAppointment={(appointmentId) => {
+                  setAppointmentList(
+                    appointmentList.filter(
+                      (appointment) => appointmentId !== appointment.id
+                    )
+                  );
+                }}
+                appointment={appointment}
+                key={appointment.id}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
